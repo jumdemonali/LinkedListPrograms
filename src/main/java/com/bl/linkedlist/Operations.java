7389c3bbf67bad012bf8ca45c966a1ea3553c79f
jumdemonali <jumdemonali@gmail.com>
2021-09-08 20:49:30 +0530

    Added: Adding elements at start in linked list

---
+++ src/main/java/com/bl/linkedlist/Operations.java
@@ -0,0 +1,11 @@
+package com.bl.linkedlist;
+
+public class Operations {
+    public static void addElementAtStart(){
+        LinkedList linkedList= new LinkedList();
+        linkedList.push(70);
+        linkedList.push(30);
+        linkedList.push(56);
+        linkedList.print();
+    }
+}
