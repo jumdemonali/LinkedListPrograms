09765e5bfeb147eda004d71fde02586fb29d97a3
jumdemonali <jumdemonali@gmail.com>
2021-09-08 20:31:41 +0530

    Added: Created simple linked list

--- src/main/java/com/bl/linkedlist/LinkedList.java
+++ src/main/java/com/bl/linkedlist/LinkedList.java
@@ -2,7 +2,19 @@ package com.bl.linkedlist;
 
 public class LinkedList {
     public static void main(String[] args) {
-        System.out.println("Welcome to linked list program");
+        Node firstNode = new Node(56);
+        Node secondNode = new Node(30);
+        Node thirdNode = new Node(70);
+        Node head = firstNode;
+        firstNode.next = secondNode;
+        secondNode.next = thirdNode;
+        Node tail = thirdNode;
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
     }
 }
 
