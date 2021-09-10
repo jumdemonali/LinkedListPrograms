9ec6200d0faad9ac2ce3c66aea4208bb32bf5325
jumdemonali <jumdemonali@gmail.com>
2021-09-10 15:42:47 +0530

    Added: Ability to peack and pop from the stack

--- src/main/java/com/bl/stack/Stack.java
+++ src/main/java/com/bl/stack/Stack.java
@@ -1,19 +1,38 @@
 package com.bl.stack;
+
 import com.bl.linkedlist.LinkedList;
 import com.bl.linkedlist.Node;
 
 public class Stack {
-        private Node top;
-        private LinkedList linkedList;
+    private Node top;
+    private LinkedList linkedList;
 
-        public Stack() {
-            this.linkedList = new LinkedList();
-        }
+    public Stack() {
+        this.linkedList = new LinkedList();
+    }
 
-        public void push() {
-            linkedList.push(70);
-            linkedList.push(30);
-            top = linkedList.push(56);
-            linkedList.print();
+    public void push() {
+        linkedList.push(70);
+        linkedList.push(30);
+        top = linkedList.push(56);
+        linkedList.print();
+    }
+
+    public int peek() {
+        push();
+        System.out.println("Top element is " + this.top.data);
+        return this.top.data;
+    }
+
+    public void pop() {
+        System.out.println("Before");
+        push();
+        Node temp = top;
+        while (temp != null) {
+            linkedList.popFirst();
+            temp = temp.next;
         }
+        System.out.println("After");
+        linkedList.print();
+    }
 }
--- src/main/java/com/bl/stack/StackMain.java
+++ src/main/java/com/bl/stack/StackMain.java
@@ -1,15 +1,24 @@
 package com.bl.stack;
+
 import java.util.*;
+
 public class StackMain {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter 1 to add element");
+        System.out.println("Enter 2 to get top element");
+        System.out.println("Enter 3 to delete all element");
         Stack stack = new Stack();
 
         switch (sc.nextInt()) {
             case 1:
                 stack.push();
-
+                break;
+            case 2:
+                stack.peek();
+                break;
+            case 3:
+                stack.pop();
         }
     }
 }
